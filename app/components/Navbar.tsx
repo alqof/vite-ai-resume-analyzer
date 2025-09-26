ce426ba9751a584f64285610ace51427763ecf47
alqof <alqofnuraji@gmail.com>
2025-09-26 16:58:17 +0700

    update icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router";
+import { IoCloudUploadOutline } from "react-icons/io5";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { Link, useLocation } from "react-router";
 
 const Navbar = () => {
+    const location = useLocation();
+    const isUploadPage = location.pathname === "/upload";
+
     return (
         <nav className="navbar">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">REZPYAI</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
+            {location.pathname==="/" ? (
+                <Link to="/upload" className="w-fit primary-button flex items-center gap-2">
+                    <IoCloudUploadOutline/> Upload Resume
+                </Link>
+            ) : (
+                <Link to="/" className="w-fit primary-button flex items-center gap-2">
+                    <LuLayoutDashboard /> Back Home
+                </Link>
+            )}
         </nav>
     )
 }
